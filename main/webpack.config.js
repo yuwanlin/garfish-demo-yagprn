9c2bb2d3618d59a46bd09444744c154d1b097391
yuwanlin <yuwanlin@cvte.com>
2023-01-09 18:28:38 +0800

    feat: 更改主应用publishPath为静态地址，不要auto，不然刷新页面获取不到资源

--- main/webpack.config.js
+++ main/webpack.config.js
@@ -4,39 +4,39 @@ const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: './src/index',
-  mode: 'development',
-  devServer: {
-    static: path.join(__dirname, 'dist'),
-    port: 3001,
-    historyApiFallback: true,
-  },
-  output: {
-    publicPath: 'auto',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx?$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        options: {
-          presets: ['@babel/preset-react'],
-        },
-      },
+    entry: './src/index',
+    mode: 'development',
+    devServer: {
+        static: path.join(__dirname, 'dist'),
+        port: 3001,
+        historyApiFallback: true,
+    },
+    output: {
+        publicPath: 'http://localhost:3001/',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                options: {
+                    presets: ['@babel/preset-react'],
+                },
+            },
+        ],
+    },
+    plugins: [
+        // new ModuleFederationPlugin({
+        //   name: "app1",
+        //   remotes: {
+        //     app2: "app2@[app2Url]/remoteEntry.js",
+        //   },
+        //   shared: {react: {singleton: true}, "react-dom": {singleton: true}},
+        // }),
+        // new ExternalTemplateRemotesPlugin(),
+        new HtmlWebpackPlugin({
+            template: './public/index.html',
+        }),
     ],
-  },
-  plugins: [
-    // new ModuleFederationPlugin({
-    //   name: "app1",
-    //   remotes: {
-    //     app2: "app2@[app2Url]/remoteEntry.js",
-    //   },
-    //   shared: {react: {singleton: true}, "react-dom": {singleton: true}},
-    // }),
-    // new ExternalTemplateRemotesPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-    }),
-  ],
 };
--- sub/src/App.js
+++ sub/src/App.js
@@ -27,13 +27,19 @@ const App = ({ props, basename }) => {
                         paddingBottom: '1rem',
                     }}
                 >
-                    <Link to="/">Home</Link> | <Link to="/invoices">Invoices</Link> |{' '}
+                    <Link to="/">Home</Link> | <Link to="/invoices">Invoices</Link> |
                     <Link to="/expenses">Expenses</Link>
+                    <div onClick={() => window.Garfish.router.push({ path: '/sub/custom' })}>
+                        <a href="" onClick={(e) => e.preventDefault()}>
+                            使用window.Garfish.root.push
+                        </a>
+                    </div>
                 </nav>
                 <Routes>
                     <Route path="/" element={<Home msg={props.msg} />} />
                     <Route path="expenses" element={<Expenses />} />
                     <Route path="invoices" element={<Invoices />} />
+                    <Route path="custom" element={<Custom />} />
                 </Routes>
             </div>
         </BrowserRouter>
@@ -52,6 +58,14 @@ function Invoices() {
     return (
         <main style={{ padding: '1rem 0' }}>
             <h2>Invoices</h2>
+        </main>
+    );
+}
+
+function Custom() {
+    return (
+        <main style={{ padding: '1rem 0' }}>
+            <h2>Custom</h2>
         </main>
     );
 }
